421353494e7a9ca8e9534beab47d42bfddc1bf2a
Steven Giacomelli <stevegiacomelli@gmail.com>
2021-07-10 10:33:45 -0400

    Server/client side  for final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -5,94 +5,110 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-  state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
-            mode: "text",
-          sentimentOutput:[],
-          sentiment:true
-        }
-  
-  renderTextArea = ()=>{
-    document.getElementById("textinput").value = "";
-    if(this.state.mode === "url") {
-      this.setState({innercomp:<textarea rows="4" cols="50" id="textinput"/>,
-      mode: "text",
-      sentimentOutput:[],
-      sentiment:true
-    })
-    } 
+  state = {
+    innercomp: <textarea rows="4" cols="50" id="textinput" />,
+    mode: "text",
+    sentimentOutput: [],
+    sentiment: true
   }
 
-  renderTextBox = ()=>{
+  renderTextArea = () => {
     document.getElementById("textinput").value = "";
-    if(this.state.mode === "text") {
-      this.setState({innercomp:<textarea rows="1" cols="50" id="textinput"/>,
-      mode: "url",
-      sentimentOutput:[],
-      sentiment:true
-    })
+    if (this.state.mode === "url") {
+      this.setState({
+        innercomp: <textarea rows="4" cols="50" id="textinput" />,
+        mode: "text",
+        sentimentOutput: [],
+        sentiment: true
+      })
+    }
+  }
+
+  renderTextBox = () => {
+    document.getElementById("textinput").value = "";
+    if (this.state.mode === "text") {
+      this.setState({
+        innercomp: <textarea rows="1" cols="50" id="textinput" />,
+        mode: "url",
+        sentimentOutput: [],
+        sentiment: true
+      })
     }
   }
 
   sendForSentimentAnalysis = () => {
-    this.setState({sentiment:true});
+    this.setState({ sentiment: true });
     let ret = "";
     let url = ".";
 
-    if(this.state.mode === "url") {
-      url = url+"/url/sentiment?url="+document.getElementById("textinput").value;
+    if (this.state.mode === "url") {
+      url = url + "/url/sentiment?url=" + document.getElementById("textinput").value;
     } else {
-      url = url+"/text/sentiment?text="+document.getElementById("textinput").value;
+      url = url + "/text/sentiment?text=" + document.getElementById("textinput").value;
     }
     ret = axios.get(url);
-    ret.then((response)=>{
-
-      //Include code here to check the sentiment and fomrat the data accordingly
-
-      this.setState({sentimentOutput:response.data});
+    ret.then((response) => {
+      this.setState({ sentimentOutput: response.data });
       let output = response.data;
-      if(response.data === "positive") {
-        output = <div style={{color:"green",fontSize:20}}>{response.data}</div>
-      } else if (response.data === "negative"){
-        output = <div style={{color:"red",fontSize:20}}>{response.data}</div>
-      } else {
-        output = <div style={{color:"orange",fontSize:20}}>{response.data}</div>
+      if (response.data === "positive") {
+        output = <div style={{ color: "green", fontSize: 20 }}>{response.data}</div>
+      } else if (response.data === "negative") {
+        output = <div style={{ color: "red", fontSize: 20 }}>{response.data}</div>
+      } else if (response.data === "neutral") {
+        output = <div style={{ color: "yellow", fontSize: 20 }}>{response.data}</div>
       }
-      this.setState({sentimentOutput:output});
-    });
+      else {
+        output = <div style={{ fontSize: 20 }}>{response.data}</div>
+      }
+      this.setState({ sentimentOutput: output });
+    })
+      .catch(error => {
+        let output = <div style={{ fontSize: 20 }}>Error: {error.message}</div>
+        this.setState({ sentimentOutput: output });
+        console.log(error.message)
+      });
   }
 
   sendForEmotionAnalysis = () => {
-    this.setState({sentiment:false});
+    this.setState({ sentiment: false });
     let ret = "";
     let url = ".";
-    if(this.state.mode === "url") {
-      url = url+"/url/emotion?url="+document.getElementById("textinput").value;
+    if (this.state.mode === "url") {
+      url = url + "/url/emotion?url=" + document.getElementById("textinput").value;
     } else {
-      url = url+"/text/emotion/?text="+document.getElementById("textinput").value;
+      url = url + "/text/emotion/?text=" + document.getElementById("textinput").value;
     }
     ret = axios.get(url);
 
-    ret.then((response)=>{
-      this.setState({sentimentOutput:<EmotionTable emotions={response.data}/>});
-  });
+    ret.then((response) => {
+      this.setState({ sentimentOutput: <EmotionTable emotions={response.data} /> });
+    }).catch(error => {
+      let output = <div style={{ fontSize: 20 }}>Error: {error.message}</div>
+      this.setState({ sentimentOutput: output });
+      console.log(error.message)
+    });
   }
-  
+
 
   render() {
-    return (  
+    return (
       <div className="App">
-      <button className="btn btn-info" onClick={this.renderTextArea}>Text</button>
-        <button className="btn btn-dark"  onClick={this.renderTextBox}>URL</button>
-        <br/><br/>
+        <button className="btn btn-info" onClick={this.renderTextArea}>Text</button>
+        <button className="btn btn-dark" onClick={this.renderTextBox}>URL</button>
+        <br /><br />
         {this.state.innercomp}
-        <br/>
+        <br />
         <button className="btn-primary" onClick={this.sendForSentimentAnalysis}>Analyze Sentiment</button>
         <button className="btn-primary" onClick={this.sendForEmotionAnalysis}>Analyze Emotion</button>
-        <br/>
-            {this.state.sentimentOutput}
+        <br />
+        {this.state.sentimentOutput}
       </div>
     );
-    }
+  }
+
+  componentDidMount() {
+    document.title = "Sentiment Analyzer";
+  }
 }
 
 export default App;
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -6,25 +6,82 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({ apikey: api_key, }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
+
+function getEmotions(value, is_url, res) {
+    let nlu = getNLUInstance();
+    let nluParams = null;
+
+    if (is_url) {
+        nluParams = { url: value, features: { emotion: {} } };
+    }
+    else {
+        nluParams = { text: value, features: { emotion: {} } };
+    }
+
+    nlu.analyze(nluParams).then(analysisResults => {
+        res.send(analysisResults.result.emotion.document.emotion);
+    })
+        .catch(err => {
+            res.status(500).send({ error: err.toString() });
+            console.log("error:", err.toString())
+        });
+}
+
+function getSentiment(value, is_url, res) {
+    let nlu = getNLUInstance();
+    let nluParams = null;
+
+    if (is_url) {
+        nluParams = { url: value, features: { sentiment: {} } };
+    }
+    else {
+        nluParams = { text: value, features: { sentiment: {} } };
+    }
+
+    nlu.analyze(nluParams).then(analysisResults => {
+        res.send(analysisResults.result.sentiment.document.label);
+    })
+        .catch(err => {
+            res.status(500).send({ error: err.toString() });
+            console.log("error:", err.toString())
+        });
+}
+
+app.get("/", (req, res) => {
     res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+    getEmotions(req.query.url, true, res);
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/url/sentiment", (req, res) => {
+    getSentiment(req.query.url, true, res);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+    getEmotions(req.query.text, false, res);
+});
+
+app.get("/text/sentiment", (req, res) => {
+    getSentiment(req.query.text, false, res);
 });
 
 let server = app.listen(8080, () => {
